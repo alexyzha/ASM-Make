acaf63c18b0b8d6e60d2a2ad0a73499adf2e1f18
alexyzha <eta.xx07@gmail.com>
2024-11-22 14:16:54 -0800

    sim move done

--- ntuple-bot/src/tup.asm
+++ ntuple-bot/src/tup.asm
@@ -30,8 +30,8 @@ section .data
     BOUND2      equ     999
     BOUND3      equ     99
     BOUND4      equ     9
-    MOVMASK     equ     0x80000000
-
+    MASK_ON     equ     0x80000000
+    MASK_OFF    equ     0x7fffffff
 
 section .bss
     tuple       resq    17
@@ -66,50 +66,52 @@ _start:
     mov rdx, 64
     call memset
 
-    mov rbx, 0
-    mov rcx, 8192
+    mov rbx, 0                          ; seed board
+    mov rcx, 2
     LOOP:
         mov dword [board+(rbx*4)], ecx
         inc rbx
-        cmp rbx, 17
+        cmp rbx, 16
         jne LOOP
-
 
     mov rdi, board                      ; setw(x) test SPECIFIC FOR THIS SCENARIO
     call print_board
 
-
-    sub rsp, 8
-    fld1
-    fld1
-    faddp
-    fstp qword [rsp]
-    movsd xmm0, qword [rsp]
-    add rsp, 8
-    mov rbx, 0
-    mov rcx, 15
-    imul rcx, rcx
-    imul rcx, rcx
-    dec rcx
-    LOOP2:
-        mov rax, qword [tuple+(rbx*8)]
-        movsd qword [rax+(rcx*8)], xmm0
-        inc rbx
-        cmp rbx, 17
-        jne LOOP2
-    mov rdi, board
-    mov rsi, 1
-    movsd xmm3, xmm0
-    call v_ofstate                      ; HOLY SHIT VSTATE WORKS 1ST TRY LESGO
-    mov rdi, FLT_PERC                   ; withupdate works
-    call printf
-
-    mov rdi, board                      ; noupdate works too
+    mov rdi, board                      ; mov up
     mov rsi, 0
-    movsd xmm0, xmm3
-    call v_ofstate
-    mov rdi, FLT_PERC
+    call sim_move
+    mov rdi, INT_PERC
+    mov rsi, rax
     call printf
+    mov rdi, board
+    call print_board
+
+    mov rdi, board                      ; mov right
+    mov rsi, 1
+    call sim_move
+    mov rdi, INT_PERC
+    mov rsi, rax
+    call printf
+    mov rdi, board
+    call print_board
+
+    mov rdi, board                      ; mov down
+    mov rsi, 2
+    call sim_move
+    mov rdi, INT_PERC
+    mov rsi, rax
+    call printf
+    mov rdi, board
+    call print_board
+
+    mov rdi, board                      ; mov left
+    mov rsi, 3
+    call sim_move
+    mov rdi, INT_PERC
+    mov rsi, rax
+    call printf
+    mov rdi, board
+    call print_board
 
     mov rdi, tuple                      ; clean tuple
     call delete_tuples
@@ -256,6 +258,7 @@ delete_config:
 
 print_board:
     ; rdi = board
+    sub rsp, 8                          ; piss ass windows
     push rbx
     push r12
     xor rbx, rbx
@@ -264,6 +267,7 @@ print_board:
     call printf
     mov rdi, r12
     print_all:
+        sub rsp, 8                      ; who doesn't love stack alignment
         push rdi
         mov r12d, dword [rdi+(rbx*4)]
         cmp r12d, BOUND4
@@ -299,6 +303,7 @@ print_board:
         call printf
         same_row:
             pop rdi
+            add rsp, 8
             inc rbx
             cmp rbx, 16
             jne print_all
@@ -306,6 +311,7 @@ print_board:
     call printf
     pop r12
     pop rbx
+    add rsp, 8
     ret
 
 v_ofstate:
@@ -389,29 +395,47 @@ sim_move:
         sim_move_inner:
             mov rcx, qword [BASE+(rsi*8)]     ; base[action]
             mov ecx, dword [rcx+(rbx*4)]      ; base[action][i]
-            add edx, dword [DIR+(rsi*4)]      ; dir[action]
-            imul edx, r12d               ; j*dir[action]
+            mov edx, dword [DIR+(rsi*4)]      ; dir[action]
+            imul edx, r12d                    ; j*dir[action]
             add ecx, edx                      ; corr cur ind in line
             mov edx, dword [board+(rcx*4)]    ; edx = board[index]
             test edx, edx
             jz sim_move_inner_next
-            test r8, r8
+            test r8, r8                       ; nothing to combine
             jz sim_move_pb_normal
-
-
-
-
+            dec r8
+            mov r9d, dword [templn+(r8*4)]
+            inc r8
+            cmp r9d, edx                      ; combine if eq 
+            jne sim_move_pb_normal
+            dec r8
+            shl edx, 1                        ; merge tile
+            add eax, edx                      ; add score
+            or edx, MASK_ON                   ; mask 0x80...
             sim_move_pb_normal:
                 mov dword [templn+(r8*4)], edx
                 inc r8
-
-            sim_move_inner_next:
+            sim_move_inner_next:              ; next row/col
                 inc r12
                 cmp r12, 4
                 jne sim_move_inner
-        
-        ; process vec here
-
+        xor r12, r12
+        sim_move_inner_pln:
+            mov rcx, qword [BASE+(rsi*8)]
+            mov ecx, dword [rcx+(rbx*4)]      ; base[action][i]
+            mov edx, dword [DIR+(rsi*4)]
+            imul edx, r12d
+            add ecx, edx                      ; ecx = index
+            mov dword [rdi+(rcx*4)], 0
+            cmp r12, r8
+            jge sim_move_inner_pln_done
+            mov edx, dword [templn+(r12*4)]   ; edx = line[j]
+            and edx, MASK_OFF
+            mov dword [rdi+(rcx*4)], edx      ; afterstate[index] = line[j] if j < line.size()
+            sim_move_inner_pln_done:
+                inc r12
+                cmp r12, 4
+                jne sim_move_inner_pln
         inc rbx
         cmp rbx, 4
         jne sim_move_outer
